027f810fa04f1d81a78b8d45c5a15af1dd9eb834
Matheus Pereira <78914056+reirass@users.noreply.github.com>
2025-11-22 00:29:06 -0300

    Update Neovim options for terminal colors and search
    Disable termguicolors and hlsearch options.

--- nvim/lua/config/options.lua
+++ nvim/lua/config/options.lua
@@ -1,5 +1,5 @@
--- Options are automatically loaded before lazy.nvim startup
--- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
--- Add any additional options here
+vim.opt.termguicolors = false
 vim.opt.updatetime = 50
--- vim.opt.termguicolors = true
+-- vim.opt.guicursor = ""
+vim.opt.hlsearch = false
+vim.opt.incsearch = true
